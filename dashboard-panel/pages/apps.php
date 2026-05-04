1062af7123a2ab1ffc9e8eba67472354282e70f2
submepro <279419351+bingoo97@users.noreply.github.com>
2026-05-04 14:23:57 +0200

    Update storefront apps download links

--- dashboard-panel/pages/apps.php
+++ dashboard-panel/pages/apps.php
@@ -11,9 +11,9 @@ $apps = [
     [
         'name' => 'NewLook 4',
         'platform' => 'Android',
-        'description' => localization_translate($t, 'apps_newlook_description', 'NewLook IPTV player for Android devices.'),
-        'url' => $appsUrl,
-        'download_url' => '',
+        'description' => localization_translate($t, 'apps_newlook_description', 'NewLook 4 player for Android devices.'),
+        'url' => '',
+        'download_url' => 'https://tinyurl.com/3av56p84',
         'logo' => '/img/new_look.png',
         'instruction_url' => '/instruction-newlook',
     ],
@@ -21,8 +21,8 @@ $apps = [
         'name' => 'NewLook 2',
         'platform' => 'Android',
         'description' => localization_translate($t, 'apps_newlook2_description', 'NewLook 2 IPTV player for Android devices.'),
-        'url' => $appsUrl,
-        'download_url' => '',
+        'url' => '',
+        'download_url' => 'https://tinyurl.com/yc37sjfa',
         'logo' => '/img/new_look.png',
         'instruction_url' => '/instruction-newlook',
     ],
@@ -30,8 +30,8 @@ $apps = [
         'name' => 'OTT-Player',
         'platform' => 'Android / iOS / Samsung / LG',
         'description' => localization_translate($t, 'apps_ottplayer_description', 'OTTPlayer for various platforms including Smart TVs.'),
-        'url' => $appsUrl,
-        'download_url' => '',
+        'url' => '',
+        'download_url' => 'https://tinyurl.com/46c2hb3s',
         'logo' => '/img/ott/logo-ott.jpg',
         'instruction_url' => '/instruction-ott-player',
     ],
@@ -44,16 +44,33 @@ $apps = [
  //       'logo' => '/img/smart_logo_t.png',
  //       'instruction_url' => '/instruction-smart-iptv',
  //   ],
-    [
-        'name' => 'SS-IPTV',
-        'platform' => 'Samsung / LG',
-        'description' => localization_translate($t, 'apps_ssiptv_description', 'SS-IPTV for Smart TVs.'),
-        'url' => 'https://ss-iptv.com/en/users/playlist',
-        'download_url' => '',
-        'logo' => '/img/ssiptv-iptv-icon.png',
-        'instruction_url' => '/instruction-smart-iptv',
-    ],
+ //   [
+ //       'name' => 'SS-IPTV',
+ //       'platform' => 'Samsung / LG',
+ //       'description' => localization_translate($t, 'apps_ssiptv_description', 'SS-IPTV for Smart TVs.'),
+ //       'url' => 'https://ss-iptv.com/en/users/playlist',
+ //       'download_url' => '',
+ //       'logo' => '/img/ssiptv-iptv-icon.png',
+ //       'instruction_url' => '/instruction-smart-iptv',
+ //   ],
 ];
+
+$appsFallbackToGlobalUrl = false;
+
+foreach ($apps as $index => $app) {
+    $directUrl = '';
+    if (!empty($app['download_url'])) {
+        $directUrl = trim((string)$app['download_url']);
+    } elseif (!empty($app['url'])) {
+        $directUrl = trim((string)$app['url']);
+    } elseif ($appsUrl !== '') {
+        $directUrl = $appsUrl;
+        $appsFallbackToGlobalUrl = true;
+    }
+
+    $apps[$index]['url'] = $directUrl;
+    $apps[$index]['uses_global_apps_url'] = ($directUrl !== '' && $directUrl === $appsUrl && empty($app['download_url']));
+}
 
 if (!$applicationInstructionsEnabled) {
     foreach ($apps as $index => $app) {
@@ -63,6 +80,7 @@ if (!$applicationInstructionsEnabled) {
 
 $smarty->assign('apps', $apps);
 $smarty->assign('apps_url', $appsUrl);
+$smarty->assign('apps_fallback_to_global_url', $appsFallbackToGlobalUrl);
 $smarty->display('apps.tpl');
 
 ?>
--- dashboard-panel/templates/apps.tpl
+++ dashboard-panel/templates/apps.tpl
@@ -1,8 +1,8 @@
 <div class="content-box">
     <h1><a href="/"><i class="fa fa-chevron-circle-left back" aria-hidden="true"></i></a> {$t.apps_title|default:'Apps'}</h1>
     <p>{$t.apps_intro|default:'Choose an app for your device and open its official download page. After installation, use your login details or stream link from the subscription modal.'}</p>
-    {if $apps_url neq ''}
-    <p>{$t.apps_download_from|default:'You can download apps directly from'}:<br/><a href="{$apps_url}" class="btn btn-sm btn-dark mt-2" target="_blank" rel="noopener noreferrer">{$apps_url}</a></p>
+    {if $apps_fallback_to_global_url && $apps_url neq ''}
+    <p>{$t.apps_download_from|default:'You can download apps directly from'}: <a href="{$apps_url}" target="_blank" rel="noopener noreferrer">{$apps_url}</a></p>
     {/if}
     <hr/>
     <table class="table table-bordered table-striped">
@@ -25,9 +25,15 @@
                     {/if}
                 </td>
                 <td>
+                    {if $app.url neq ''}
                     <a href="{$app.url}" target="_blank" rel="noopener noreferrer" class="text-dark">
                         <i class="fa fa-download" aria-hidden="true"></i>
                     </a>
+                    {else}
+                    <span class="text-muted">
+                        <i class="fa fa-download" aria-hidden="true"></i>
+                    </span>
+                    {/if}
                 </td>
             </tr>
         {/foreach}
